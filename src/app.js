d989ea1f2f3482b30032edcbd2b5f530ddd244b8
Kevin Stradtman <stradtkt22@gmail.com>
2020-03-23 02:10:23 -0400

    Added a couple of modules

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import {http} from './http';
+import {ui} from './ui';
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
-};
-
-getData('https://jsonplaceholder.typicode.com/posts');
+//Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts);
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+    .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
