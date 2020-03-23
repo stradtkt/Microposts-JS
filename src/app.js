0aa6fa2d291c2eb7ed8b5f40e76e2f8d41bb31f0
Kevin Stradtman <stradtkt22@gmail.com>
2020-03-23 03:01:47 -0400

    Created an alert system

--- src/app.js
+++ src/app.js
@@ -1,10 +1,30 @@
 import {http} from './http';
 import {ui} from './ui';
 
-//Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
+    .catch(err => console.log(err));
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+  
+  const data = {
+    title,
+    body
+  }
+
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert("Post Added", "alert alert-success");
+      ui.clearFields();
+      getPosts();
+
+    })
     .catch(err => console.log(err));
 }
